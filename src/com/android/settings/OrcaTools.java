4d5471b9fc2d19f1b4f43fd04d09041f586faf7c
Orca Project <shamukillaz@orcaproject.co>
2013-06-02 23:59:55 -0400

    Pie Updates

--- src/com/android/settings/OrcaTools.java
+++ src/com/android/settings/OrcaTools.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 Orca Project 
+ * Credits To Root-Box Project 2012
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
