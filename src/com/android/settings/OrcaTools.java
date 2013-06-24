c1518e008911bc4e33572df1245f2b070c5979be
Orca Project <shamukillaz@orcaproject.co>
2013-06-24 00:34:23 -0400

    Revert "Orca Tools Overhaul (may revert)"
    This reverts commit 67ba4f4729c9e5dc6593a1eff13c5b6b3bd68bd9.

--- src/com/android/settings/OrcaTools.java
+++ src/com/android/settings/OrcaTools.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Orca Project (June 22 OVERHAULED By Orca Project)
+ * Copyright (C) 2013 Orca Project 
  * Credits To Root-Box Project 2012
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
